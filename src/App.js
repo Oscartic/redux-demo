40a47d4022ca39776e059d58a3551f20746224f8
Oscar Ballesteros <oscartic@gmail.com>
2023-05-12 00:56:16 -0400

    recoil way

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { useState } from 'react'
 import Accumulator from './Components/Accumulator'
 import Btn from './Components/Btn'
-import React from './images/react.png'
+import { useRecoilState } from 'recoil';
+import { counterState } from './state';
+import Recoil from './images/recoil.png'
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useRecoilState(counterState);
 
-  const incrementHandler = () => {
-    setCount(prevState => prevState + 1);
-  }
-  const decrementHandler = () => {
-    if (count <= 0) return;
-    setCount(prevState => prevState - 1);
-  }
+  const increment = () => setCount(count + 1);
+  const decrement = () => { 
+    if (count === 0) return;
+    setCount(count - 1);
+  };
   return (
     <main className="main">
-      <img src={React} alt="Zustand logo" className="option-state"/>
-    <h1 className="title">CountApp</h1>
+      <img src={Recoil} alt="Zustand logo" className="option-state"/>
+    <h1 className="title">CountApp with Recoil</h1>
     <div className="card">
       <Accumulator count={count}/>
       <div className="btnContainer">
-        <Btn handler={decrementHandler} text="-"/>
-        <Btn handler={incrementHandler} text="+"/>
+        <Btn handler={decrement} text="-"/>
+        <Btn handler={increment} text="+"/>
       </div>
     </div>
   </main>
